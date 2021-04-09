9d3fb0c08f2c42950599da288881ebc01c6f6c59
SonicYeager <kimstone.yeager@gmail.com>
2021-04-09 15:26:40 +0200

    Final Config Step: - added test proj; - edited any CMakeLists.txt to use the shared lib (dll); - wrote a empty test with major inlcudes;

--- qtbackend/qtbackend/internaltypes.h
+++ qtbackend/qtbackend/internaltypes.h
@@ -3,10 +3,10 @@
 #include <vector>
 #include <functional>
 
-#include <Qt>
-#include <QtCore>
-#include <QtNetwork/QTcpServer>
-#include <QtNetwork/QTcpSocket>
+//#include <Qt>
+//#include <QtCore>
+//#include <QtNetwork/QTcpServer>
+//#include <QtNetwork/QTcpSocket>
 
 template<typename ... Args>
 using Event = std::function<void(Args ...)>;
---
+++ tool_tests/tests_Interactor.cpp
@@ -0,0 +1,10 @@
+#include "../qtbackend/qtbackend/interactor.h"
+#include "../qtbackend/qtbackend/internaltypes.h"
+#include "gtest.h"
+
+TEST(TestInteractor, func_in_out)
+{
+    EXPECT_TRUE(true);
+}
+
+
